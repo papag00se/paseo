97b2334bf97402469f18862965cc579dfa5fcd72
papag00se <jlanderson@gmail.com>
2026-09-08 13:53:26 -0700

    fix(git): expose file actions in changes tree

--- packages/app/src/git/diff-pane.tsx
+++ packages/app/src/git/diff-pane.tsx
@@ -1438,6 +1438,9 @@ function ChangedFilesTree({
           revealTargetName={mode.revealTargetName}
           onDownload={mode.onDownload}
           onDuplicate={mode.onDuplicate}
+          onStage={mode.onStage}
+          onUnstage={mode.onUnstage}
+          onStash={mode.onStash}
           onRevert={mode.onRevert}
           testID={`diff-tree-file-${item.fileIndex}`}
         />
